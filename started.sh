ddcf11190004497013901455cbc482dc84b679e6
minowa-develop <minowa@tatsu.uk>
2024-03-20 22:00:41 +0900

    init

---
+++ started.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+THUNDERBIRD_PATH="/home/minowa/ダウンロード/thunderbird-115.8.1/thunderbird/thunderbird"
+
+case "${1}" in
+  "start")
+    "${THUNDERBIRD_PATH}" &
+    ;;
+  "stop")
+    kill $(pgrep thunderbird) &
+    ;;
+  *)
+    echo "not option: ${1}. please input [start or stop]"
+    exit 1
+esac
