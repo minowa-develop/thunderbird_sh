831bde55f65ed4ffa8084cb753bb28398614d515
minowa-develop <minowa@tatsu.uk>
2024-03-26 22:33:06 +0900

    停止中にstopした時のメッセージ追加

--- started.sh
+++ started.sh
@@ -1,13 +1,28 @@
 #!/bin/bash
 
-THUNDERBIRD_PATH="/home/minowa/ダウンロード/thunderbird-115.8.1/thunderbird/thunderbird"
+THUNDERBIRD_PATH="/home/minowa/tools/thunderbird/thunderbird"
+
+function start_app {
+  "${THUNDERBIRD_PATH}" > /dev/null 2>&1 &
+  exit $?
+}
+
+function stop_app {
+  kill $(pgrep thunderbird) > /dev/null 2>&1 || echo "not running." 
+  exit $?
+}
 
 case "${1}" in
   "start")
-    "${THUNDERBIRD_PATH}" > /dev/null 2>&1 &
+    start_app
     ;;
   "stop")
-    kill $(pgrep thunderbird) &
+    stop_app
+    ;;
+  "restart")
+    stop_app
+    sleep 5
+    start_app
     ;;
   *)
     echo "not option: ${1}. please input [start or stop]"
